6878b8631014f0033641249a330533ba9018ded6
Fabio <fabio@hobbytek.net>
2017-09-16 11:33:01 +0200

    First Commit

---
+++ KeePassFingerprintPlugin/FingerprintPlugin/FingerprintPlugin.cs
@@ -0,0 +1,18 @@
+﻿using KeePass.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FingerprintPlugin
+{
+	public sealed class FingerprintPlugin : Plugin
+	{
+		private IPluginHost m_host = null;
+
+		public override bool Initialize(IPluginHost host)
+		{
+			m_host = host;
+			return true;
+		}
+	}
+}
